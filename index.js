ff64d6ef952466d2e02a6a3708947e01479e85ec
Ani Vardanyan <anivardanyan@Anis-MacBook-Pro.local>
2025-02-20 18:30:30 +0400

    task 1.2 let counter

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 console.log("Hello, JavaScript Training!");
-const userName="Ani";
+// Task 1.1
+const userName = "Ani";
 console.log(userName);
+
+// Task 1.2
+let counter = 0;
+console.log(counter);
+
+
+
