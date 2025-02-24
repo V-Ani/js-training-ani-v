f48474350d215eeb8665859679ac3690580045c6
Ani Vardanyan <anivardanyan@Anis-MacBook-Pro.local>
2025-02-24 11:26:04 +0400

    Task 2: Use typeof to Check Data Types

--- VariablesAndDataTypes.js
+++ VariablesAndDataTypes.js
@@ -19,5 +19,31 @@ console.log(userAge);
 const colours=["white", "Black", "Green"];
 console.log(typeof colours)
 
+// Task 2.1
+let num = 5; 
+console.log(typeof num);
 
+// Task 2.2
+let str = "Hello, world!";
+console.log(typeof str); 
+
+// Task2.3
+let bool = true; 
+console.log(typeof bool); 
+
+// Task 2.4 
+let n = null; 
+console.log(typeof n); 
+
+// Task 2.5
+let und; 
+console.log(typeof und); 
+
+// Task 2.6
+let sym = Symbol('symbol'); 
+console.log(typeof sym); 
+
+// Task 2.7
+let bigIntValue = 1234567890123456789012345678901234567890n; 
+console.log(typeof bigIntValue); 
 
