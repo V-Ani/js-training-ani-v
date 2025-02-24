6456e6ca3c05700ef22753ffab46da43a6662d00
Ani Vardanyan <anivardanyan@Anis-MacBook-Pro.local>
2025-02-24 11:11:23 +0400

    Task 1: Identify When to Use let or const

---
+++ VariablesAndDataTypes.js
@@ -0,0 +1,23 @@
+// Task 1.1
+const userName = "Ani";
+console.log(userName);
+
+// Task 1.2
+let counter = 0;
+console.log(counter);
+
+// Task 1.3
+const pi = Math.PI
+console.log(pi);
+
+// Task 1.4
+let userAge;
+userAge=40;
+console.log(userAge);
+
+// Task 1.5
+const colours=["white", "Black", "Green"];
+console.log(typeof colours)
+
+
+
