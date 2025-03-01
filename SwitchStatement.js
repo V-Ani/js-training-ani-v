06130d6fb3d5902ed1a79bc0962608c7c03c4bd2
Ani Vardanyan <anivardanyan@Anis-MacBook-Pro.local>
2025-03-01 12:03:53 +0400

    Task 2: Season Finder

---
+++ SwitchStatement.js
@@ -0,0 +1,24 @@
+let monthName = "March"; 
+switch (month) {
+  case "December":
+  case "January":
+  case "February":
+    console.log(`${month} Winter`);
+    Break;
+  case "March":
+  case "April":
+  case "May":
+    console.log(`${month} Spring`);
+    Break;
+  case "June":
+  case "July":
+  case "August":
+    console.log(`${month} Summer`);
+    Break;
+case "September":
+  case "October":
+  case "November":
+    console.log(`${month} Autumn`);
+    Break;
+ default:
+    console.log("Invalid month! Please enter a valid month name."); 
--- index.js
+++ index.js
@@ -3,3 +3,43 @@ const myInput = userInput("Please provide number:");
 console.log(myInput);
 
 // If the input number is 1, it should log "Monday".
+
+// You are building a scheduling application that requires converting a numeric input (1-7) into the corresponding day of the week. The application should handle the following:
+
+// If the input number is 1, it should log "Monday".
+// If the input number is 2, it should log "Tuesday".
+// If the input number is 3, it should log "Wednesday".
+// If the input number is 4, it should log "Thursday".
+// If the input number is 5, it should log "Friday".
+// If the input number is 6, it should log "Saturday".
+// If the input number is 7, it should log "Sunday".
+// If the input number is outside the range of 1-7, it should return an error message: "Invalid day number. Please enter a number between 1 and 7."
+// The program should be able to process this input and output the correct day, or display an error message if the number is invalid.
+
+      let dayName;
+      
+      switch(dayNumber) {
+        case 1:
+          dayName = "Monday";
+          break;
+        case 2:
+          dayName = "Tuesday";
+          break;
+        case 3:
+          dayName = "Wednesday";
+          break;
+        case 4:
+          dayName = "Thursday";
+          break;
+        case 5:
+          dayName = "Friday";
+          break;
+        case 6:
+          dayName = "Saturday";
+          break;
+        case 7:
+          dayName = "Sunday";
+          break;
+        default:
+          dayName = "Invalid day number. Please enter a number between 1 and 7.";
+      }
