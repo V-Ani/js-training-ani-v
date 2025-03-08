83535e9d1f76ac2fe61d4d6f9e40f2e0b343d05e
Ani Vardanyan <anivardanyan@Anis-MacBook-Pro.local>
2025-03-08 20:37:24 +0400

    Task 1. While Loop Task – Reverse Counting

---
+++ Loops.js
@@ -0,0 +1,13 @@
+// 1. While Loop Task – Reverse Counting
+// Requirements:
+
+// Start from 10 and count down to 1.
+// Use a while loop.
+// Print each number in the console.
+
+let num = 10;
+while (num >= 1) {
+  console.log(num);
+  num--; 
+}
+
