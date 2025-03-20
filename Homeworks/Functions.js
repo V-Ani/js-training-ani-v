3dec48ee4b6653ec9cf838fa389c5644bf2f1cb5
Ani Vardanyan <anivardanyan@Anis-MacBook-Pro.local>
2025-03-20 15:54:32 +0400

    Task 2: Function Expression

--- Homeworks/Functions.js
+++ Homeworks/Functions.js
@@ -3,9 +3,28 @@
 // The function should accept two parameters: 'firstName' and 'lastName',
 // and return a full name by combining both parameters with a space in between.
 
-function getFullName(firstName, lastName) {
-    return firstName + " " + lastName;
-  }
 
-  let fullName = getFullName("Ani", "Vardanyan")
-  console.log(fullName) 
+
+// function getFullName(firstName, lastName) {
+//     return firstName + " " + lastName;
+//   }
+
+//   let fullName = getFullName("Ani", "Vardanyan")
+//   console.log(fullName) 
+
+// Task 2: Function Expression
+// Requirement: Declare a function using a function expression.
+// The function should accept a number as a parameter and return whether the number is even or odd.
+
+  const checkEvenOrOdd = function(num) {
+    if (num % 2 === 0) {
+      return "Even";
+    } else {
+      return "Odd";
+    }
+  };
+  
+  console.log(checkEvenOrOdd(4)); 
+  console.log(checkEvenOrOdd(7));  
+
+  
