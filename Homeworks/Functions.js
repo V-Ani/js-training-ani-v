d2fd11cd517a953bc6ebd80bc69a1e131311365c
Ani Vardanyan <anivardanyan@Anis-MacBook-Pro.local>
2025-03-20 15:39:27 +0400

    Task 1: Function Declaration

---
+++ Homeworks/Functions.js
@@ -0,0 +1,11 @@
+// Task 1: Function Declaration
+// Requirement: Declare a function using the function declaration syntax.
+// The function should accept two parameters: 'firstName' and 'lastName',
+// and return a full name by combining both parameters with a space in between.
+
+function getFullName(firstName, lastName) {
+    return firstName + " " + lastName;
+  }
+
+  let fullName = getFullName("Ani", "Vardanyan")
+  console.log(fullName) 
