4e84b3dfdea77130a8570d20732482a33aab7eb8
Ani Vardanyan <anivardanyan@Anis-MacBook-Pro.local>
2025-03-20 16:45:00 +0400

    Task 3: Default Value with an Optional Parameter

--- Homeworks/Functions.js
+++ Homeworks/Functions.js
@@ -16,15 +16,27 @@
 // Requirement: Declare a function using a function expression.
 // The function should accept a number as a parameter and return whether the number is even or odd.
 
-  const checkEvenOrOdd = function(num) {
-    if (num % 2 === 0) {
-      return "Even";
-    } else {
-      return "Odd";
-    }
-  };
+  // const checkEvenOrOdd = function(num) {
+  //   if (num % 2 === 0) {
+  //     return "Even";
+  //   } else {
+  //     return "Odd";
+  //   }
+  // };
   
-  console.log(checkEvenOrOdd(4)); 
-  console.log(checkEvenOrOdd(7));  
+  // console.log(checkEvenOrOdd(4)); 
+  // console.log(checkEvenOrOdd(7));  
 
-  
+// Task 3: Default Value with an Optional Parameter
+// Requirement: Create a function that accepts a 'discount' parameter for an item price.
+// If no 'discount' is provided, it should default to 0%. The function should calculate and return the final price after applying the discount.
+
+
+function calculateFinalPrice(price, discount = 0) {
+  let finalPrice = price - (price * (discount / 100));
+  return finalPrice;
+}
+
+console.log(calculateFinalPrice(48));  
+console.log(calculateFinalPrice(48, 10));  
+
