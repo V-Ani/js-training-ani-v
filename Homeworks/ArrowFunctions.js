565feda1fa18908e569f8615e140467cfe1b4747
Ani Vardanyan <anivardanyan@Anis-MacBook-Pro.local>
2025-03-28 16:09:23 +0400

    Task2: Arrow Function with Default Parameters

--- Homeworks/ArrowFunctions.js
+++ Homeworks/ArrowFunctions.js
@@ -16,15 +16,29 @@
 //   }
 
 // Arrow function for multiply
-const multiply = (a, b) => a * b;
+// const multiply = (a, b) => a * b;
 
-// Arrow function for cube
-const cube = (n) => n * n * n;
+// // Arrow function for cube
+// const cube = (n) => n * n * n;
 
-// Arrow function for sayHello
-const sayHello = (name) => `Hello, ${name}!`;
+// // Arrow function for sayHello
+// const sayHello = (name) => `Hello, ${name}!`;
 
-// Test the functions
-console.log(multiply(2, 3));  // Output: 6
-console.log(cube(3));         // Output: 27
-console.log(sayHello("John")); // Output: Hello, John!
+// // Test the functions
+// console.log(multiply(2, 3));  // Output: 6
+// console.log(cube(3));         // Output: 27
+// console.log(sayHello("John")); // Output: Hello, John!
+
+// Task 2. Arrow Function with Default Parameters
+// Requirement:
+// Create an arrow function greet that takes a name parameter.
+// The parameter should have a default value of "Guest".
+// The function should return "Hello, [name]!".
+
+const greet = (name = "Guest") => {
+    return `Hello, ${name}!`;
+  };
+
+console.log(greet());       // Output: "Hello, Guest!"
+console.log(greet("Alice")); // Output: "Hello, Alice!"
+
