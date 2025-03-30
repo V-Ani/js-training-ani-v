507d9ff53c7df1a350de081dbec4cace96ca75b0
Ani Vardanyan <anivardanyan@Anis-MacBook-Pro.local>
2025-03-30 14:27:34 +0400

    Task 1: Get String Length

---
+++ Homeworks/Strings.js
@@ -0,0 +1,15 @@
+// Task 1: Get String Length
+// Requirement: Write a function that receives
+// a string as input and returns the length of the string.
+
+function getStringLength(str) {
+    return str.length;
+}
+
+// Example usage:
+console.log(getStringLength("Hello, world!")); // Output: 13
+
+
+
+
+
